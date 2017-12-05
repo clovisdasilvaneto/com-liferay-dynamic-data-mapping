195175c128f6fc9465a5ed832b312b80d1839291
Clovis Neto <clovis.neto@liferay.com>
2017-12-05 11:43:03 -0300

    LPS-75358 Remove col-empty class after field render

--- dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder.js
+++ dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder.js
@@ -1243,6 +1243,7 @@ AUI.add(
 
 								activeLayout.normalizeColsHeight(new A.NodeList(row));
 								field.get('container').append(instance._getFieldActionsLayout());
+								field.get('container').ancestor('.col').removeClass('col-empty');
 							}
 						);
 
